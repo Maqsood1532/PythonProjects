a3f64097190557ef2ba1a19525e0cf90be3b826e
Maqsood Choudhary <MaqsoodC@quinnox.com>
2020-05-29 19:05:16 +0530

    Added simple page using view rendering

--- ReportingApp/views.py
+++ ReportingApp/views.py
@@ -1,6 +1,17 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from datetime import datetime
 # Create your views here.
 
 def index(request):
-    return HttpResponse("Hello, Django!")
+    now = datetime.now()
+
+    return render(
+            request,
+            'ReportingApp/index.html',
+            {
+                'title': "Reporting App",
+                'message': " Hello Maqsood",
+                'content': " on " + now.strftime("%A, %d %B, %Y at %X")
+            }
+        )
